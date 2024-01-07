1721e6101971eafb640ed77dbb5dc7f64443d0a8
anthonydinino <adinino1996@gmail.com>
2024-01-07 20:30:47 +1030

    Exercise 5.10 complete

--- src/App.jsx
+++ src/App.jsx
@@ -116,9 +116,11 @@ const App = () => {
       <Togglable buttonLabel="create new blog">
         <BlogForm createBlog={createBlog} />
       </Togglable>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} refreshBlogs={refreshBlogs} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} refreshBlogs={refreshBlogs} />
+        ))}
     </div>
   );
 };
