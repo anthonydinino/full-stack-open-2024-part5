34c541036e3922a1896b73e0a315c9343011f7bf
anthonydinino <adinino1996@gmail.com>
2024-01-08 18:59:16 +1030

    Exercise 5.15 complete

--- src/tests/Blog.test.js
+++ src/tests/Blog.test.js
@@ -5,9 +5,7 @@ import Blog from "../components/Blog";
 import userEvent from "@testing-library/user-event";
 
 describe("<Blog/>", () => {
-  let container;
-
-  beforeEach(() => {
+  test("renders and shows title and author but not URL or likes", () => {
     const blog = {
       user: { name: "Test User" },
       title: "Component testing",
@@ -15,10 +13,7 @@ describe("<Blog/>", () => {
       likes: 2,
       url: "https://test.com/blogs/1",
     };
-    container = render(<Blog blog={blog} />).container;
-  });
-
-  test("renders and shows title and author but not URL or likes", () => {
+    const container = render(<Blog blog={blog} />).container;
     expect(container).toHaveTextContent("Component testing Anthony");
     const title = screen.queryByText("2", { exact: false });
     const url = screen.queryByText("https://test.com/blogs/1", {
@@ -29,6 +24,14 @@ describe("<Blog/>", () => {
   });
 
   test("url and likes are shown when button is clicked", async () => {
+    const blog = {
+      user: { name: "Test User" },
+      title: "Component testing",
+      author: "Anthony",
+      likes: 2,
+      url: "https://test.com/blogs/1",
+    };
+    render(<Blog blog={blog} />);
     const user = userEvent.setup();
     const button = screen.queryByText("view");
     await user.click(button);
@@ -38,5 +41,24 @@ describe("<Blog/>", () => {
     });
     expect(title).not.toBeNull();
     expect(url).not.toBeNull();
+  });
+
+  test("when like button is clicked, prop function is recevied twice", async () => {
+    const blog = {
+      user: { name: "Test User" },
+      title: "Component testing",
+      author: "Anthony",
+      likes: 2,
+      url: "https://test.com/blogs/1",
+    };
+    const mockHandler = jest.fn();
+    render(<Blog blog={blog} addLike={mockHandler} />);
+    const user = userEvent.setup();
+    const button = screen.queryByText("view");
+    await user.click(button);
+    const likeBtn = screen.queryByText("like");
+    await user.click(likeBtn);
+    await user.click(likeBtn);
+    expect(mockHandler.mock.calls).toHaveLength(2);
   });
 });
