ce826a12ce2250d83b7e0453835ce72042e5bdb8
anthonydinino <adinino1996@gmail.com>
2024-01-08 15:22:15 +1030

    Exercise 5.14 complete

--- src/tests/Blog.test.js
+++ src/tests/Blog.test.js
@@ -2,25 +2,41 @@ import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Blog from "../components/Blog";
+import userEvent from "@testing-library/user-event";
 
-test("renders and shows title and author but not URL or likes", () => {
-  const blog = {
-    title: "Component testing",
-    author: "Anthony",
-    likes: 2,
-    url: "https://test.com/blogs/1",
-  };
+describe("<Blog/>", () => {
+  let container;
 
-  const { container } = render(<Blog blog={blog} />);
-
-  const element = screen.getByText("Component testing Anthony", {
-    exact: false,
+  beforeEach(() => {
+    const blog = {
+      user: { name: "Test User" },
+      title: "Component testing",
+      author: "Anthony",
+      likes: 2,
+      url: "https://test.com/blogs/1",
+    };
+    container = render(<Blog blog={blog} />).container;
   });
 
-  const div = container.querySelector(".blog");
-  expect(div).toHaveTextContent("Component testing Anthony");
-  const title = screen.queryByText("2", { exact: false });
-  const url = screen.queryByText("https://test.com/blogs/1", { exact: false });
-  expect(title).toBeNull();
-  expect(url).toBeNull();
+  test("renders and shows title and author but not URL or likes", () => {
+    expect(container).toHaveTextContent("Component testing Anthony");
+    const title = screen.queryByText("2", { exact: false });
+    const url = screen.queryByText("https://test.com/blogs/1", {
+      exact: false,
+    });
+    expect(title).toBeNull();
+    expect(url).toBeNull();
+  });
+
+  test("url and likes are shown when button is clicked", async () => {
+    const user = userEvent.setup();
+    const button = screen.queryByText("view");
+    await user.click(button);
+    const title = screen.queryByText("2", { exact: false });
+    const url = screen.queryByText("https://test.com/blogs/1", {
+      exact: false,
+    });
+    expect(title).not.toBeNull();
+    expect(url).not.toBeNull();
+  });
 });
