80b20dbd054f6e9c4b7c5cd2cfbe8311e470d3f8
anthonydinino <adinino1996@gmail.com>
2024-01-09 18:37:15 +1030

    Exercise 5.17 complete

--- src/App.jsx
+++ src/App.jsx
@@ -101,6 +101,7 @@ const App = () => {
           <div>
             username
             <input
+              id="username"
               type="text"
               value={username}
               name="Username"
@@ -110,6 +111,7 @@ const App = () => {
           <div>
             password
             <input
+              id="password"
               type="password"
               value={password}
               name="Password"
