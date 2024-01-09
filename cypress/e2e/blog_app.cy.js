6da2bece11f50e44a3b5f44012755b2232c8bfd1
anthonydinino <adinino1996@gmail.com>
2024-01-09 23:44:36 +1030

    Exercise 5.18 complete

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,3 +1,4 @@
+/// <reference types="Cypress" />
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
@@ -8,5 +9,32 @@ describe("Blog app", function () {
     cy.get("form").should("have.descendants", "input");
     cy.contains("username").should("be.visible");
     cy.contains("password").should("be.visible");
+  });
+
+  describe("Login", function () {
+    beforeEach(function () {
+      const user = {
+        username: "adinino",
+        name: "Anthony Dinino",
+        password: "password",
+      };
+      cy.request("POST", "http://localhost:3003/api/users", user);
+    });
+    it("succeeds with correct credentials", function () {
+      cy.get("#username").type("adinino");
+      cy.get("#password").type("password");
+      cy.contains("login").get("button[type='submit']").click();
+      cy.contains("adinino logged in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get("#username").type("adinino");
+      cy.get("#password").type("wrongpassword");
+      cy.contains("login").get("button[type='submit']").click();
+      cy.get(".notification").should("contain", "wrong username or password");
+      cy.get(".notification").should("have.css", "border-style", "solid");
+      cy.get(".notification").should("have.css", "color", "rgb(255, 0, 0)");
+      cy.get("html").should("not.contain", "adinino logged in");
+    });
   });
 });
